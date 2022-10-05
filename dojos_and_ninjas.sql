2fe55d690ffb6f982fa38278c7d45fb66936f534
Jordan <jyodan90@gmail.com>
2022-10-05 00:01:38 -0700

    Add files via upload

---
+++ dojos_and_ninjas.sql
@@ -0,0 +1,43 @@
+-- SELECT * from dojos;
+-- INSERT INTO dojos
+-- (Name)
+-- VALUES ("Glendale");
+
+-- INSERT INTO dojos
+-- (Name)
+-- VALUES ("Inglewood");
+
+-- INSERT INTO dojos
+-- (Name)
+-- VALUES ("Chicago");
+
+
+-- REACTIVE CODE ABOVE OR BELOW SEPERATELY 
+
+
+-- DELETE FROM dojos
+-- -- WHERE (id) = (1);
+
+-- -- DELETE FROM dojos
+-- -- WHERE (id) = (2);
+
+-- -- DELETE FROM dojos
+-- -- WHERE (id) = (3);
+
+
+
+
+INSERT INTO dojos
+(Name)
+VALUES ("Glendale");
+
+INSERT INTO dojos
+(Name)
+VALUES ("Inglewood");
+
+INSERT INTO dojos
+(Name)
+VALUES ("Chicago");
+
+SELECT * from dojos;
+
